bdcecad1c3f93bd43f8e03a0ddd5d33b0a10fc18
Rickard Garnau <rickard.garnau89@gmail.com>
2026-03-09 11:11:02 +0100

    fix: add FastAPI app instance to make compose build work

--- app/main.py
+++ app/main.py
@@ -1,28 +1,24 @@
 import json
-
-from fastapi import FastAPI, HTTPException
+import os
 from contextlib import asynccontextmanager
-
+from fastapi import FastAPI
 from kafka import KafkaProducer
 from psycopg_pool import ConnectionPool
 from psycopg.rows import dict_row
-
-import os
-
-from starlette import status
-
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
-
-# Kafka Setup
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS", "kafka:9092")
-#PRODUCTS_TOPIC = os.getenv("PRODUCTS_TOPIC", "products.created")
 
-DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+pool = ConnectionPool(DATABASE_URL)
+
+app = FastAPI(title="FoodHub API")
+
+@app.get("/")
+def read_root():
+    return {"message": "FoodHub is running"}
