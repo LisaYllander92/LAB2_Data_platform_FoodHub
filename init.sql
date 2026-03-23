7b5f7f6d361255bf506ef3dc0f9d5d791ff564c7
LisaYllander92 <Lisaylander92@gmail.com>
2026-03-23 09:46:04 +0100

    fix: create table insted of alter table on curated_recipe

--- init.sql
+++ init.sql
@@ -6,17 +6,16 @@ CREATE TABLE IF NOT EXISTS staging_recipes (
 
 CREATE TABLE IF NOT EXISTS curated_recipes (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(250) NOT NULL UNIQUE, 
+    title VARCHAR(250) NOT NULL UNIQUE,
     image TEXT,
     cooking_minutes INT,
     servings INT,
     instructions TEXT,
+    ingredients TEXT,
+    ingredients_normalized TEXT,
+    cached_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-ALTER TABLE curated_recipes ADD COLUMN ingredients TEXT;
-ALTER TABLE curated_recipes ADD COLUMN IF NOT EXISTS cached_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE curated_recipes ADD COLUMN IF NOT EXISTS ingredients_normalized TEXT;
 
 CREATE TABLE IF NOT EXISTS ingredients (
     ingred_id SERIAL PRIMARY KEY,
